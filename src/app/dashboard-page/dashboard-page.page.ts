234d82327ae9502770e70ffda15ec6caf6365794
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2020-10-30 00:32:04 -0400

    Dashboard and forget password back page

--- src/app/dashboard-page/dashboard-page.page.ts
+++ src/app/dashboard-page/dashboard-page.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { AuthenticationService } from "../shared/authentication-service";
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
   selector: 'app-dashboard-page',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPagePage implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthenticationService,
+    public afStore: AngularFirestore,
+    public ngFireAuth: AngularFireAuth,) { }
+
+  @Input()  name:string;
 
   ngOnInit() {
-  }
+    var userData=JSON.parse(localStorage.getItem('user'));
+    //console.log(userData.email);
 
+    var test=this.afStore.collection('users',ref => ref.where('email', '==', userData.email)).snapshotChanges();
+   
+
+        test.subscribe(data=>{
+
+          var posts = data.map(e => {
+            return {
+              id: e.payload.doc.id,
+              fisrtName: e.payload.doc.data()['firstName'],
+              lastName: e.payload.doc.data()['lastName'],
+            };
+          })
+          //console.log(posts);
+          this.name= posts[0].fisrtName+" "+posts[0].lastName
+          
+         });
+
+
+      
+        
+          
+      }
+      
+Signout(){
+
+    this.authService.SignOut()
+     
+
+  }
 }
